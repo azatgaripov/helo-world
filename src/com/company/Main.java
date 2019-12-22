f3950aa814f3cecc2ce243a3f32a0c49c2bad290
azatgaripov <azatgaripov@gmail.com>
2019-12-22 18:46:54 +0400

    helo world

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String helo="Helo";
+        String world="World";
+        System.out.println(helo + " "+world);
+
+    }
+}
